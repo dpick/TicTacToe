48fc8b7f55f7ac04a6aec8ee7a5a399a24674f29
David Pick <pickdavid@gmail.com>
2010-11-09 16:24:20 -0500

    updates to computer player spec

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -5,27 +5,13 @@ describe "computer player tests" do
     @player = ComputerPlayer.new('x')
   end
 
-  describe "open? tests" do
-    it "should return 1 for a list of nil" do
-      @player.open?([nil, nil, nil]).should == 1
-    end
-    
-    it "should return 1 for a list with only x and nil in it" do
-      @player.open?([nil, 'x', nil]).should == 1
+  describe "toggle_symbol tests" do
+    it "should return O for X" do
+      @player.toggle_symbol('X').should == 'O'
     end
 
-    it "should be 0 for a list with o in it" do
-      @player.open?([nil, 'o', 'x']).should == 0
-    end
-
-    it "should return 0 for a list with only o in it" do
-      @player.open?(['o'] * 3).should == 0
-    end
-  end
-
-  describe "available_wins_heuristic tests" do
-    it "should return 4 for an empty board and the center square" do
-      @player.available_wins_heuristic(1,1).should == 4
+    it "should return X for O" do
+      @player.toggle_symbol('O').should == 'X'
     end
   end
 end
